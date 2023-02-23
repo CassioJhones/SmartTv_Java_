13ed03bf03a8f1689d31448b5411505f907bfde3
Cassio Jhones <cassio.bjhones@gmail.com>
2023-02-23 19:11:01 -0300

    coments

--- src/SmartTv.java
+++ src/SmartTv.java
@@ -1,39 +1,43 @@
 public class SmartTv {
+    // --DECLARAÇÃO DAS VARIAVEIS E TIPOS--
     boolean ligada = false;
     int canal = 1;
     int volume = 25;
+     // --DECLARAÇÃO DAS VARIAVEIS E TIPOS--
 
+    // ----ON / OFF----ON / OFF----
     public void ligar() {
         ligada = true;
     }
-
     public void desligar() {
         ligada = false;
     }
+    // ----ON / OFF----ON / OFF----
 
+    // ----CONTROLE DE VOLUME----CONTROLE DE VOLUME----
     public void aumentarVolume() {
-        volume++;
+        volume ++;
         System.out.println("--Aumentou para: " + volume);
     }
-
     public void diminuirVolume() {
-        volume--;
+        volume --;
         System.out.println("--Diminuiu para: " + volume);
     }
+    // ----CONTROLE DE VOLUME----CONTROLE DE VOLUME----
 
+    // ----CONTROLE DE CANAL----CONTROLE DE CANAL----
     public void subirCanal() {
-        canal++;
+        canal ++;
         System.out.println("--Canal alterada para: " + canal);
     }
-
     public void descerCanal() {
-        canal--;
+        canal --;
         System.out.println("--Canal alterada para: " + canal);
     }
-
     public void mudarCanal(int novoCanal){
         canal = novoCanal;
     }
+     // ----CONTROLE DE CANAL----CONTROLE DE CANAL----
 
 
 
