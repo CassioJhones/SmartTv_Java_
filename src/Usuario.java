b9946b43112a943df66ff7c01398e7e77fa2a1cb
Cassio Jhones <cassio.bjhones@gmail.com>
2023-03-07 13:30:34 -0300

    Update Usuario.java

--- src/Usuario.java
+++ src/Usuario.java
@@ -5,24 +5,28 @@ public class Usuario {
         smartTv.aumentarVolume();
         smartTv.aumentarVolume();
 
-        System.out.println("--TV Ligada? "+smartTv.ligada);
-        System.out.println("--Canal Atual: "+smartTv.canal);
-        System.out.println("--Volume Atual: "+smartTv.volume);
+        System.out.println("--TV Ligada? " + smartTv.ligada);
+        System.out.println("--Canal Atual: " + smartTv.canal);
+        System.out.println("--Volume Atual: " + smartTv.volume);
 
         smartTv.ligar();
-        System.out.println("--TV Ligada? "+smartTv.ligada);
+        System.out.println("--TV Ligada? " + smartTv.ligada);
 
         smartTv.desligar();
-        System.out.println("--TV Ligada? "+smartTv.ligada);
+        System.out.println("--TV Ligada? " + smartTv.ligada);
 
         smartTv.mudarCanal(15);
-        System.out.println("--Canal Atual: "+smartTv.canal);
+        System.out.println("--Canal Atual: " + smartTv.canal);
+
         smartTv.subirCanal();
-        System.out.println("--Canal Atual: "+smartTv.canal);
+        System.out.println("--Canal Atual: " + smartTv.canal);
+        
         smartTv.diminuirVolume();
         smartTv.diminuirVolume();
         smartTv.diminuirVolume();
         smartTv.diminuirVolume();
-        System.out.println("--Volume Atual: "+smartTv.volume);
+        System.out.println("--Volume Atual: " + smartTv.volume);
+
     }
 }
+
